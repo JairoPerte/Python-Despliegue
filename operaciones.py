9639dc8c82830e63495c6f6718b8b3b38aeddcf8
JairoPerte <jairopertecarras@gmail.com>
2024-11-25 12:39:48 +0100

    Falta paso de cadena a entero/float

--- operaciones.py
+++ operaciones.py
@@ -1,37 +1,50 @@
 # Función sumar
 def sumar(num1,num2):
-    if (type(num1) is int or type(num1) is float) and (type(num2) is int or type(num2) is float):
-        return num1+num2
-    else:
+    num1=convertir(num1)
+    num2=convertir(num2)
+    if(num1 is False or num2 is False):
         return False
+    return num1+num2
 
 # Función restar
 def restar(num1,num2):
-    if (type(num1) is int or type(num1) is float) and (type(num2) is int or type(num2) is float):
-        return num1-num2
-    else:
+    num1=convertir(num1)
+    num2=convertir(num2)
+    if(num1 is False or num2 is False):
         return False
+    return num1-num2
 
 # Función multiplicar
 def multiplicar(num1,num2):
-    if (type(num1) is int or type(num1) is float) and (type(num2) is int or type(num2) is float):
-        resultado=0
-        for i in range(1,num2):
-            resultado+=num1
-        return resultado
-    else:
+    num1=convertir(num1)
+    num2=convertir(num2)
+    if(num1 is False or num2 is False):
         return False
+    resultado=0
+    for i in range(1,num2):
+        resultado+=num1
+    return resultado
 
-
+# Función dividir
 def dividir(num1, num2):
-    if ((type(num1) is int or type(num1) is float) and (type(num2) is int or type(num2) is float) and num2 != 0):
-        resto = num1
-        contador = 0
-        while resto >= num2:
-            resto -= num2
-            contador = contador +1
-        return contador
-    else:
+    num1=convertir(num1)
+    num2=convertir(num2)
+    if(num1 is False or num2 is False):
         return False
+    resto = num1
+    contador = 0
+    while resto >= num2:
+        resto -= num2
+        contador = contador +1
+    return contador
 
-
+# Convierte y si tiene una excepción pues significa que es false  
+def convertir(num):
+    try:
+        int(num)
+    except ValueError:
+        try:
+            float(num)
+        except ValueError:
+            return False
+    return num
