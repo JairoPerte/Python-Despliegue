3fcd5eee645a1e280ec1cd026f5ad536adbfeea9
JairoCerecedaBerciano <mmjrz451@gmail.com>
2024-11-27 18:59:50 +0100

    Factorial iterativo

--- menu.py
+++ menu.py
@@ -3,16 +3,20 @@ import operaciones
 # Función para mostrar el menú
 def mostrar_menu():
    opcion=0
-   while(opcion<1 or opcion>5):
-      print("1- sumar, 2- restar, 3- multiplicar, 4-dividir y 5- salir")
+   while(opcion<1 or opcion>6):
+      print("1- sumar, 2- restar, 3- multiplicar, 4-dividir, 5- salir y 6- Factorial iterativo")
       opcion = int(input("¿Qué opción elige? "))
    opciones(opcion)
 
 # Función para mostrar la opcción
 def opciones(opcion):
-   if(opcion!=5):
+   if(opcion!=5 and opcion !=6):
       num1 = input("¿Cuál es el primer número 1? ")
       num2 = input("¿Cuál es el primer número 2? ")
+
+   if(opcion is 6):
+      num1 = input("¿Cuál es el número? ")
+
    match opcion:
       case 1:
          solucion=operaciones.sumar(num1,num2)
@@ -23,6 +27,8 @@ def opciones(opcion):
       case 4:
          if(num2 != "0"):
             solucion=operaciones.dividir(num1,num2)
+      case 6:
+         solucion=operaciones.factorial_iterativo(num1)
 
    if(opcion != 5):
       try:
